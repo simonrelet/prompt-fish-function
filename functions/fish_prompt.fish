db5a089c05b2ac4432ca79999fde4aaaf6b1a1bf
Simon Relet <simonrelet@users.noreply.github.com>
2017-07-03 12:53:25 +0200

    change color for root user

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,32 +1,36 @@
 function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+  set -l last_status $status
 
   # User
-  set_color $fish_color_user
+  set -l me (whoami)
+  if [ "$me" = "root" ]
+    set_color $fish_color_user_root
+  else
+    set_color $fish_color_user
+  end
   printf '%s' (whoami)
   set_color normal
 
   # PWD
   set_color $fish_color_cwd
-	printf ':'
-
-	set -l dir (dirname (prompt_pwd))
-	if [ $dir != '.' ]
-		printf '%s/' $dir
-	end
-	set_color normal
-
-	set_color $prompt_color_current_folder
-	printf '%s' (basename (prompt_pwd))
+  printf ':'
+  set -l dir (dirname (prompt_pwd))
+  if [ $dir != '.' ]
+    printf '%s/' $dir
+  end
   set_color normal
 
-	basic_git_prompt
+  set_color $prompt_color_current_folder
+  printf '%s' (basename (prompt_pwd))
+  set_color normal
+
+  basic_git_prompt
   printf '\n'
 
   if not test $last_status -eq 0
     set_color $fish_color_error
-	else
-		set_color $prompt_color_arrow
+  else
+    set_color $prompt_color_arrow
   end
 
   printf '➤ '
@@ -34,7 +38,7 @@ function fish_prompt --description 'Write out the prompt'
 end
 
 function fish_right_prompt --description 'Write out the right prompt'
-	set_color $fish_color_cwd
-	printf '%s\n' (date '+%Y-%m-%d %H:%M:%S')
-	set_color normal
+  set_color $fish_color_cwd
+  printf '%s\n' (date '+%Y-%m-%d %H:%M:%S')
+  set_color normal
 end
